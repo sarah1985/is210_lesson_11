af05a0002c0da6b752976380f69fc7652b6da807
sarah1985 <sarahmleavy@gmail.com>
2014-11-15 22:11:08 -0500

    weather

--- current_weather.py
+++ current_weather.py
@@ -22,23 +22,24 @@ class CurrentWeather(object):
 
     zip_codes = {}
     base_url = 'http://api.openweathermap.org/data/2.5/weather'
-    
     def __init__(self, zipcode_data='zipcode_database.csv'):
-        pass
+        self.read_csv(zipcode_data)
 
     def get_weather(self, city, country, units='metric'):
         """get weather"""
 
-        self.api_query = '{}?units={}&q={},{}'.format(
-            base_url, units, city, country
+        api_query = '{}?units={}&q={},{}'.format(
+        base_url, units, city, country
         )
 
         try:
-            self.response = urllib2.urlopen(api_query)
+            response = urllib2.urlopen(api_query)
         except urllib2.HTTPError, error:
             raise CurrentWeatherException(
                 error.code,
                 'Error: {} {}'.format(error.code, error.msg))
+
+        return json.load(response)['main']
 
     def read_csv(self, csv_path):
         """csv reader"""
@@ -49,7 +50,7 @@ class CurrentWeather(object):
                 input_file = csv.reader(open(csv_path, 'r'))
 
                 for line in input_file:
-                    zip_codes[line[0]] = {
+                    csv_path[line[0]] = {
                         'city': line[1],
                         'state': line[2],
                         'latitude': float(line[3]),
@@ -69,7 +70,6 @@ class CurrentWeather(object):
             raise CurrentWeatherException(
                 9010, 'CSV zipcode database {} not found'.format(csv_path))
 
-
     def get_city_by_zipcode(self, zipcode):
         """get city with zip code"""
 
@@ -82,7 +82,7 @@ class CurrentWeather(object):
     def get_weather_by_zipcode(self, zipcode):
         """weather by zipcode"""
 
-    #return self.get_weather(self.get_city_by_zipcode(zipcode), 'US')
+        return self.get_weather(self.get_city_by_zipcode(zipcode), 'US')
 
 # print get_weather('New York', 'us')
 # print get_weather('San Francisco', 'us')
